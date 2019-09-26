420ee1168d3365768501d856443130385feea205
Ujjwal Gandhi <31807351+ujjwalgandhi@users.noreply.github.com>
2019-09-26 15:52:04 +0530

    Changes

--- lab-5/RegFile.v
+++ lab-5/RegFile.v
@@ -3,8 +3,17 @@ module RegFile(clk,reset,ReadReg1,ReadReg2,WriteData,WriteReg,RegWrite,ReadData1
 	input [1:0] ReadReg1, ReadReg2, WriteReg;
 	input [31:0] WriteData;
 	output [31:0] ReadData1, ReadData2;
-	wire [3:0] decout;
+	wire [3:0] decout, regin, regout;
 	
 	decoder2_4 d(decout, WriteData);
+	genvar i;
+	generate 
+		for(i=0; i<4;i=i+1) begin: loop
+			assign regin[i] = clk & RegWrite & decout[i];
+			reg_32bit r(regout, WriteData, regin[i], reset);
+		end
+	endgenerate
+	bit32_4to1mux m1();
+	bit32_4to1mux m2();
 	
 endmodule
